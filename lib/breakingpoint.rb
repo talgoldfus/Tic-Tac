a135c2d02a43fe09a656e7c158d13af7ba933246
Tal Goldfus <talgoldfus@gmail.com>
2016-06-14 20:17:04 -0400

    Smarter computer 2 with one breaking point

---
+++ lib/breakingpoint.rb
@@ -0,0 +1,22 @@
+Where would you like to move? Choose box 1 - 9:
+4
+   | O | X 
+-----------
+ X | X |   
+-----------
+ O | X | O 
+Computer is choosing his next move
+ O | O | X 
+-----------
+ X | X |   
+-----------
+ O | X | O 
+Where would you like to move? Choose box 1 - 9:
+6
+ O | O | X 
+-----------
+ X | X | X 
+-----------
+ O | X | O 
+Congratulations! Tal won this round
+
